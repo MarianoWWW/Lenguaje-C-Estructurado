66863681e7e54c45f8121838372b03b8f9340d9e
MARIANO <175405662+MarianoWWW@users.noreply.github.com>
2026-04-16 04:54:42 -0300

    Delete 3.3 directory

--- 3.3/carga.h
+++
@@ -1,6 +0,0 @@
-#ifndef CARGA_H_INCLUDED
-#define CARGA_H_INCLUDED
-
-int Carga(FILE *arch, t_producto *producto);
-
-#endif // CARGA_H_INCLUDED
--- 3.3/modificar.h
+++
@@ -1,6 +0,0 @@
-#ifndef MODIFICAR__H_INCLUDED
-#define MODIFICAR__H_INCLUDED
-
-int Modificar(t_producto *producto, int cant);
-
-#endif // MODIFICAR__H_INCLUDED
